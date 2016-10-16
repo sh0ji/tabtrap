de0f9e3ea201a94e791c7536839f7ab7ff48bf14
sh0ji <sh0ji@users.noreply.github.com>
2016-10-16 18:00:35 -0400

    jQuery now optional

--- src/tabtrap.js
+++ src/tabtrap.js
@@ -1,231 +1,226 @@
 /**
  * --------------------------------------------------------------------------
- * Tabtrap (v1.1.4): tabtrap.js
+ * Tabtrap (v2.0.0): tabtrap.js
  * by Evan Yamanishi
  * Licensed under GPL-3.0
  * --------------------------------------------------------------------------
  */
 
-const Tabtrap = (($) => {
+/**
+ * ------------------------------------------------------------------------
+ * Constants
+ * ------------------------------------------------------------------------
+ */
 
-    /**
-     * ------------------------------------------------------------------------
-     * Constants
-     * ------------------------------------------------------------------------
-     */
+const NAME = 'tabtrap'
+const VERSION = '2.0.0'
+const DATA_KEY = 'tabtrap'
 
-    const NAME = 'tabtrap'
-    const VERSION = '1.1.4'
-    const DATA_KEY = 'a11y.tabtrap'
-    const EVENT_KEY = `.${DATA_KEY}`
-    const JQUERY_NO_CONFLICT = $.fn[NAME]
-    const ESCAPE_KEYCODE = 27
-    const TAB_KEYCODE = 9
+const KEYCODE = {
+    ESCAPE: 27,
+    TAB: 9
+}
 
-    const Default = {
-        disableOnEscape: false,
-        tabbableElements: [
-            'a[href]:not([tabindex="-1"])',
-            // area must be descendant of a named map
-            'map[name] area[href]:not([tabindex="-1"])',
-            'input:not([disabled]):not([tabindex="-1"])',
-            'select:not([disabled]):not([tabindex="-1"])',
-            'textarea:not([disabled]):not([tabindex="-1"])',
-            'button:not([disabled]):not([tabindex="-1"])',
-            'iframe:not([tabindex="-1"])',
-            'object:not([tabindex="-1"])',
-            'embed:not([tabindex="-1"])',
-            '[tabindex]:not([tabindex="-1"])',
-            '[contentEditable=true]:not([tabindex="-1"])'
-        ].join(',')
+const Default = {
+    disableOnEscape: false,
+    tabbableElements: [
+        'a[href]:not([tabindex="-1"])',
+        'map[name] area[href]:not([tabindex="-1"])',
+        'input:not([disabled]):not([tabindex="-1"])',
+        'select:not([disabled]):not([tabindex="-1"])',
+        'textarea:not([disabled]):not([tabindex="-1"])',
+        'button:not([disabled]):not([tabindex="-1"])',
+        'iframe:not([tabindex="-1"])',
+        'object:not([tabindex="-1"])',
+        'embed:not([tabindex="-1"])',
+        '[tabindex]:not([tabindex="-1"])',
+        '[contentEditable=true]:not([tabindex="-1"])'
+    ]
+}
+
+const DefaultType = {
+    disableOnEscape: 'boolean',
+    tabbableElements: 'object'
+}
+
+const Event = {
+    KEYDOWN_DISABLE: `keydown.disable.${DATA_KEY}`,
+    KEYDOWN_TAB: `keydown.tab.${DATA_KEY}`
+}
+
+const jQueryAvailable = window.jQuery !== undefined
+
+
+/**
+ * ------------------------------------------------------------------------
+ * Class Definition
+ * ------------------------------------------------------------------------
+ */
+
+class Tabtrap {
+
+    constructor(element, config) {
+        this.el = element
+        this.enabled = true
+        this.config = this._getConfig(config)
+        this.tabbable = this._getTabbable()
+
+        this._createEventListener()
+        if (this.config.disableOnEscape) this._setEscapeEvent()
+        console.log(this)
     }
 
-    const DefaultType = {
-        disableOnEscape: 'boolean',
-        tabbableElements: 'string'
+
+    // getters
+
+    static get NAME() {
+        return NAME
     }
 
-    const Event = {
-        KEYDOWN_DISABLE : `keydown.disable${EVENT_KEY}`,
-        KEYDOWN_TAB     : `keydown.tab${EVENT_KEY}`,
-        TAB_PRESS       : `tab${EVENT_KEY}`
+    static get VERSION() {
+        return VERSION
+    }
+
+    static get DATA_KEY() {
+        return DATA_KEY
+    }
+
+    static get KEYCODE() {
+        return KEYCODE
+    }
+
+    static get Default() {
+        return Default
+    }
+
+    static get DefaultType() {
+        return DefaultType
+    }
+
+    static get Event() {
+        return Event
+    }
+
+    static get jQueryAvailable() {
+        return jQueryAvailable
     }
 
 
-    /**
-     * ------------------------------------------------------------------------
-     * Class Definition
-     * ------------------------------------------------------------------------
-     */
+    // public
 
-    class Tabtrap {
+    enable() {
+        this.enabled = true
+    }
 
-        constructor(element, config) {
-            this._isEnabled = true
-            this._element   = element
-            this._jQueryUI  = (typeof $.ui === 'undefined') ? false : true
-            this._config    = this._getConfig(config)
-            this._$tabbable = this._getTabbable()
+    disable() {
+        this.enabled = false
+    }
 
-            this._manageFocus()
-            this._setEscapeEvent()
+    toggle() {
+        this.enabled = !this.enabled
+    }
+
+
+    // private
+
+    _getConfig(config) {
+        return Object.assign({},
+            this.constructor.Default,
+            config
+        )
+    }
+
+    _getTabbable() {
+        return this.el.querySelectorAll(this.config.tabbableElements.join(','))
+    }
+
+    _getKeyCode(event) {
+        return event.which || event.keyCode || 0
+    }
+
+    _createEventListener() {
+        if (jQueryAvailable) {
+            jQuery(this.el).off(Event.KEYDOWN_TAB)
+            jQuery(this.el).on(Event.KEYDOWN_TAB, (e) => this._manageFocus(e))
+        } else {
+            this.el.addEventListener('keydown', (e) => this._manageFocus(e))
         }
+    }
 
-
-        // getters
-
-        static get VERSION() {
-            return VERSION
-        }
-
-        static get Default() {
-            return Default
-        }
-
-        static get NAME() {
-            return NAME
-        }
-
-        static get DATA_KEY() {
-            return DATA_KEY
-        }
-
-        static get Event() {
-            return Event
-        }
-
-        static get EVENT_KEY() {
-            return EVENT_KEY
-        }
-
-        static get DefaultType() {
-            return DefaultType
-        }
-
-
-        // public
-
-        enable() {
-            this._isEnabled = true
-        }
-
-        disable() {
-            this._isEnabled = false
-        }
-
-        toggle() {
-            this._isEnabled = !this._isEnabled
-        }
-
-        dispose() {
-            $.removeData(this._element, DATA_KEY)
-
-            $(document).off(EVENT_KEY)
-
-            this._isEnabled = null
-            this._element   = null
-            this._jQueryUI  = null
-            this._config    = null
-            this._$tabbable = null
-        }
-
-
-        // private
-
-        _getConfig(config) {
-            return $.extend({},
-                this.constructor.Default,
-                $(this._element).data(),
-                config
-            )
-        }
-
-        _getTabbable() {
-            return this._jQueryUI ?
-                $(this._element).find(':tabbable') :
-                $(this._element).find(this._config.tabbableElements);
-        }
-
-        _manageFocus() {
-            $(this._element)
-                .off(Event.KEYDOWN_TAB)
-                .on(Event.KEYDOWN_TAB, (event) => {
-                    if (!this._isEnabled && !$(this._element).is(':visible')) return true
-                    if (event.which === TAB_KEYCODE) {
-                        $(document).trigger(Event.TAB_PRESS)
-                        let tabIndex = this._$tabbable.index(event.target)
-                        let conditions = {
-                            outside: tabIndex < 0,
-                            wrapForward: tabIndex === this._$tabbable.length - 1 && !event.shiftKey,
-                            wrapBackward: tabIndex === 0 && event.shiftKey
-                        }
-                        if (conditions.outside || conditions.wrapForward) {
-                            event.preventDefault()
-                            this._$tabbable.first().focus()
-                        }
-                        if (conditions.wrapBackward) {
-                            event.preventDefault()
-                            this._$tabbable.last().focus()
-                        }
-                    }
-                })
-        }
-
-        _setEscapeEvent() {
-            if (this._config.disableOnEscape && $(this._element).is(':visible')) {
-                $(this._element).on(Event.KEYDOWN_DISABLE, (event) => {
-                    if (event.which === ESCAPE_KEYCODE) {
-                        this.disable()
-                    }
-                })
+    _manageFocus(e) {
+        if (this._getKeyCode(e) === KEYCODE.TAB && this.enabled) {
+            let tabIndex = Array.from(this.tabbable).indexOf(e.target)
+            let condition = {
+                outside: tabIndex < 0,
+                wrapForward: tabIndex === this.tabbable.length - 1 && !e.shiftKey,
+                wrapBackward: tabIndex === 0 && e.shiftKey
+            }
+            if (condition.outside || condition.wrapForward) {
+                e.preventDefault()
+                this.tabbable[0].focus()
+            }
+            if (condition.wrapBackward) {
+                e.preventDefault()
+                this.tabbable[this.tabbable.length - 1].focus()
             }
         }
+    }
 
-
-        // static
-
-        static _jQueryInterface(config) {
-            return this.each(function() {
-                let data    = $(this).data(DATA_KEY)
-                let _config = typeof config === 'object' ?
-                    config : null
-
-                if (!data && /destroy|hide/.test(config)) {
-                    return
-                }
-
-                if (!data) {
-                    data = new Tabtrap(this, _config)
-                    $(this).data(DATA_KEY, data)
-                }
-
-                if (typeof config === 'string') {
-                    if (data[config] === undefined) {
-                        throw new Error(`No method named "${config}"`)
-                    }
-                    data[config]()
-                }
-            })
-        }
-
+    _setEscapeEvent() {
+        this.el.addEventListener(Event.KEYDOWN_DISABLE, (e) => {
+            if (this._getKeyCode(e) === KEYCODE.ESCAPE) {
+                this.disable()
+            }
+        })
     }
 
 
-    /**
-     * ------------------------------------------------------------------------
-     * jQuery
-     * ------------------------------------------------------------------------
-     */
+    // static
 
-    $.fn[NAME]             = Tabtrap._jQueryInterface
-    $.fn[NAME].Constructor = Tabtrap
-    $.fn[NAME].noConflict  = function() {
-        $.fn[NAME] = JQUERY_NO_CONFLICT
+    static _jQueryInterface(config) {
+        return this.each(function() {
+            let data = jQuery(this).data(DATA_KEY)
+            let _config = typeof config === 'object' ?
+                config : null
+
+            if (!data && /disable/.test(config)) {
+                return
+            }
+
+            if (!data) {
+                data = new Tabtrap(this, _config)
+                jQuery(this).data(DATA_KEY, data)
+            }
+
+            if (typeof config === 'string') {
+                if (data[config] === undefined) {
+                    throw new Error(`No method named "${config}"`)
+                }
+                data[config]()
+            }
+        })
+    }
+
+    static trap(element, config) {
+        return new Tabtrap(element, config)
+    }
+
+}
+
+
+/**
+ * ------------------------------------------------------------------------
+ * jQuery
+ * ------------------------------------------------------------------------
+ */
+if (jQueryAvailable) {
+    const JQUERY_NO_CONFLICT = jQuery.fn[NAME]
+    jQuery.fn[NAME] = Tabtrap._jQueryInterface
+    jQuery.fn[NAME].Constructor = Tabtrap
+    jQuery.fn[NAME].noConflict = function() {
+        jQuery.fn[NAME] = JQUERY_NO_CONFLICT
         return Tabtrap._jQueryInterface
     }
-
-    return Tabtrap
-
-})(jQuery)
+}
 
 export default Tabtrap
